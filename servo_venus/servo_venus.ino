9ec919ed2d91453a259ed80c8874367c5be60ed7
MitchelBrands <m.a.j.brands@student.tue.nl>
2017-05-12 10:21:29 +0200

    initial code

---
+++ servo_venus/servo_venus.ino
@@ -0,0 +1,41 @@
+// Robotics with the BOE Shield - ForwardTenSeconds
+// Make the BOE Shield-Bot roll forward for ten seconds, then stop.
+
+#include <Servo.h>                           // Include servo library
+ 
+Servo servoLeft;                             // Declare left and right servos
+Servo servoRight;
+Servo servoGrab;
+
+
+ 
+void setup()                                 // Built-in initialization block
+{
+
+}  
+ 
+void loop()                                  // Main loop auto-repeats
+{     
+  servoLeft.attach(13);                      // Attach left signal to pin 13
+  servoRight.attach(12);                     // Attach right signal to pin 12
+  servoGrab.attach(11);
+  
+
+  // Full speed forward
+  servoLeft.writeMicroseconds(1700);         // Left wheel counterclockwise
+  servoRight.writeMicroseconds(1700);        // Right wheel clockwise
+  delay(1000);
+
+  
+  
+  servoGrab.writeMicroseconds(300);
+  delay(1000);
+  servoGrab.writeMicroseconds(3000);
+  delay(1000);
+
+  
+  
+    servoLeft.detach();                        // Stop sending servo signals
+  servoRight.detach();                    // Empty, nothing needs repeating
+  servoGrab.detach();
+}
---
+++ utraturn_venus/utraturn_venus.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>   
+Servo servoUltrasound;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  servoUltrasound.attach(10);
+  servoUltrasound.write(0);
+  delay(1000);
+  for (int i=0; i<175; i=i+1){
+  servoUltrasound.attach(10);
+  servoUltrasound.write(i);
+  delay(10);
+  }
+  
+ 
+}
