0c9554ca30a8700fe952c4fd5a1dece837b17f95
Niek Brekelmans <niek.brekelmans@gmail.com>
2017-05-16 14:05:38 +0200

    Converted to library version of the Encoder part

---
+++ venus.ino
@@ -0,0 +1,19 @@
+#include <Encoder.h>
+
+//Encoder encoder(7,8);
+Encoder encoder(D6, D6);
+
+void setup() {
+	Serial.begin(9600);
+}
+
+void loop() {
+	float reading[2];
+	encoder.readEncoder(reading);
+	encoder.updateRelativePosition(reading);
+	encoder.checkDistanceDriven(0, 0, 0);
+
+	Serial.print("reading: ");
+	Serial.println(reading[0]);
+
+}
