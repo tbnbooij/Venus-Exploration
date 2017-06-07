1216d5a595b6c5a194cf9e1ade452f24cccf60f6
MitchelBrands <m.a.j.brands@student.tue.nl>
2017-06-07 15:37:45 +0200

    main algorithm

--- venus.ino
+++ venus.ino
@@ -1,9 +1,18 @@
+/*0: searching rocks
+1: found a rock
+2: return to base (not turned)
+3: return to base (turned towards base w/ relative position)
+4: return to base (found beacon), finding entrance
+5: on the ramp*/
+
+
 #include <Encoder.h> 
 #include <Motion.h> 
 #include <Wireless.h> 
 #include <IR.h>
 
 const int robot = 1;
+int robotStatus = 0;
 
 Encoder encoder(robot); 
 Motion motion(robot); 
@@ -21,6 +30,17 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  encoder.updateRelativePosition();
+  int ultrasoundAngle = motion.measureUltrasound();
+  switch(robotStatus){
+    case 0:
+    case 2:
+    case 3:
+      if(ultrasoundAngle == -1){
+        motion.startDriving();  
+      }
+      else {
+        motion.turnAfterObstacle(utrasoundAngle);
+      }
+  }
 }
