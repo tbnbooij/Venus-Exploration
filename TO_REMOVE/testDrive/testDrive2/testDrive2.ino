fa4c0ef630e9137872ae1a5bf5ca697ff2ae9dd8
MitchelBrands <m.a.j.brands@student.tue.nl>
2017-06-16 09:36:28 +0200

    new test

---
+++ TO_REMOVE/testDrive/testDrive2/testDrive2.ino
@@ -0,0 +1,48 @@
+#include <Motion.h>
+//#include <Encoder.h>
+#include <IR.h>
+
+Motion motion(1);
+//Encoder encoder(2);
+IR ir(1);
+
+void setup() {
+  // put your setup code here, to run once:
+  motion.setup();
+  ir.setup();
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+
+  int lineDetection = ir.readLineSensor();
+
+  if (lineDetection == 1){
+      motion.stopDriving();
+      motion.turnRight();
+  }
+  else if (lineDetection == 2){
+      motion.stopDriving();
+      motion.turnLeft();
+  }
+  else if (lineDetection == 3){
+      motion.stopDriving();
+      motion.turnRight();
+  }
+  else if (lineDetection == 0){
+      
+  }
+  
+  int turnOrNot = motion.measureUltrasound();
+  if (turnOrNot < 0 && lineDetection == 0){
+    motion.startDriving();   
+  }
+  else{
+    
+    motion.turnAfterObstacle(turnOrNot);
+    
+    }
+
+}
