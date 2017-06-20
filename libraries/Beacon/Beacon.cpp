72d45166b3e5419385a1702fe81a75a1f9da6c38
Thomas Booij <t.b.n.booij@student.tue.nl>
2017-06-20 21:57:54 +0200

    Update Beacon.cpp

--- libraries/Beacon/Beacon.cpp
+++ libraries/Beacon/Beacon.cpp
@@ -4,15 +4,15 @@
 
 Beacon::Beacon(uint8_t pin) {
   LDRPin = pin;
-  pinMode(LDRPin, INPUT);
 }
 
 void Beacon::start() {
+  pinMode(LDRPin, INPUT);
   rAve[ROLL_AVE_SIZE] = {0};
   maxVal[2] = {0};
 }
 
-void Beacon::measure(int angle, int left, int right, Servo servo, bool demo) {
+int Beacon::measure(int angle, int left, int right, Servo servo) {
   rAve[angle % ROLL_AVE_SIZE] = (short) analogRead(LDRPin);
   short newAve = 0;
   for(uint8_t i = 0; i < ROLL_AVE_SIZE; i++) {
@@ -26,28 +26,10 @@ void Beacon::measure(int angle, int left, int right, Servo servo, bool demo) {
   }
 
   if(angle == left || angle == right) {
-    float ratio = ((float) (maxVal[1] - right))/((float) (left-right));
-    /*
-    if(ratio == 0.5) {
-      Serial.println("RIGHT IN FRONT OF IT!");
-    }
-    else if (ratio < 0.5) {
-      Serial.println("TO THE RIGHT!");
-    }
-    else {
-      Serial.println("TO THE LEFT!");
-    }*/
-    if(demo) {
-      servo.write(maxVal[1]);
-      delay(2000);
-      if(angle == left) {
-        servo.write(left);
-      }
-      else {
-        servo.write(right);
-      }
-      delay(1000);
-    }
+    uint8_t maxAng = maxVal[1];
+
     start();
+    return maxAng;
   }
+  return -1;
 }
